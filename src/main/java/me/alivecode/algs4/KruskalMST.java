20f87773de525c11eaf3cddfa2be4f7d2e8506db
oujt <kevinojt@hotmail.com>
2017-02-26 21:05:25 +0800

    Implements the Kruskal algorithm for determining minimum spanning tree.

---
+++ src/main/java/me/alivecode/algs4/KruskalMST.java
@@ -0,0 +1,74 @@
+package me.alivecode.algs4;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
+/**
+ * The {@code KruskalMST} class implements the Kruskal algorithm
+ * for determining minimum spanning tree.
+ */
+public class KruskalMST {
+    private Queue<Edge> mst;
+    private double weight;
+
+    /**
+     * Determines the minimum spanning tree of the specified
+     * edge weighted graph {@code G}.
+     *
+     * @param G the graph.
+     */
+    public KruskalMST(EdgeWeightedGraph G) {
+        mst = new Queue<>();
+
+        //edu.princeton.cs.algs4.MinPQ<Edge> pq = new edu.princeton.cs.algs4.MinPQ<Edge>();
+        MinPQ<Edge> pq = new MinPQ<>();
+        for(Edge e: G.edges()) {
+            pq.insert(e);
+        }
+        UF uf = new UF(pq.size());
+        while(!pq.isEmpty() && mst.size() < G.V() - 1) {
+            Edge e = pq.delMin();
+            int v = e.either();
+            int w = e.other(v);
+            if (!uf.connected(v, w)) {
+                mst.enqueue(e);
+                weight += e.weight();
+                uf.union(v, w);
+            }
+        }
+    }
+
+    /**
+     * Returns the minimum spanning tree.
+     *
+     * @return the minimum spanning tree
+     */
+    public Iterable<Edge> mst() {
+        return mst;
+    }
+
+    /**
+     * Returns the weight of the mst.
+     *
+     * @return the weight of the mst
+     */
+    public double weight() {
+        return weight;
+    }
+
+    // unit test code
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        KruskalMST mst = new KruskalMST(G);
+
+        StdOut.println("mst: ");
+        for(Edge e: mst.mst()){
+            StdOut.println(e);
+        }
+        StdOut.printf("%.5f\n", mst.weight());
+    }
+
+}
