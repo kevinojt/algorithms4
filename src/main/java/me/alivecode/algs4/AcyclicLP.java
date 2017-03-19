9a83509db63a0c35400e12f9648a6506970b2d73
oujt <kevinojt@hotmail.com>
2017-03-19 15:17:07 +0800

    Add AcyclicLP class for determining longest path in acyclic edge-weighted digraph.

--- src/main/java/me/alivecode/algs4/AcyclicLP.java
+++ src/main/java/me/alivecode/algs4/AcyclicLP.java
@@ -4,25 +4,32 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * The {@code AcyclicSP} class determines the shortest path
+ * The {@code AcyclicLP} class determines the longest path
  * in acyclic edge-weighted digraphs.
+ *
  */
-public class AcyclicSP {
-    private final DirectedEdge[] edgeTo; // edgeTo[v] = previous edge to v
-    private final double[] distTo; // distTo[v] = distance to v
+public class AcyclicLP {
+    private final DirectedEdge[] edgeTo;
+    private final double[] distTo;
 
-    public AcyclicSP(EdgeWeightedDigraph G, int s) {
+    /**
+     * Determines longest path in the specified digraph {@code G}.
+     *
+     * @param G the digraph
+     * @param s the source vertex
+     */
+    public AcyclicLP(EdgeWeightedDigraph G, int s) {
         edgeTo = new DirectedEdge[G.V()];
         distTo = new double[G.V()];
-        for (int v = 0; v < G.V(); v++) {
-            distTo[v] = Double.POSITIVE_INFINITY;
+        for(int v = 0; v < G.V(); v++) {
+            distTo[v] = Double.NEGATIVE_INFINITY;
         }
         validateVertex(s);
         distTo[s] = 0.0;
 
         Topological topological = new Topological(G);
         if (!topological.hasOrder()) {
-            throw new IllegalArgumentException("the specified graph is not acyclic");
+            throw new IllegalArgumentException("the specified edge-weighted digraph is not acyclic");
         }
         // relax vertices in topological order
         // so that no edges point back to vertices
@@ -32,65 +39,59 @@ public class AcyclicSP {
                 relax(e);
             }
         }
+
     }
 
-    // relax directed edge e
+    // update if e has longer distance to w
     private void relax(DirectedEdge e) {
         int v = e.from(), w = e.to();
-        if (distTo[v] + e.weight() < distTo[w]) {
-            distTo[w] = distTo[v] + e.weight();
+        if (distTo[v] + e.weight() > distTo[w]) {
             edgeTo[w] = e;
+            distTo[w] = distTo[v] + e.weight();
         }
     }
 
     private void validateVertex(int v) {
-        int V = edgeTo.length;
-        if (v < 0 || v >= V)
+        int V = distTo.length;
+        if (v < 0 || v >= V) {
             throw new IllegalArgumentException(v + " is not between 0 and " + (V-1));
+        }
     }
 
     /**
-     * Is there any path from source vertex to v?
+     * Is there any path from the source vertex to {@code v}?
      * @param v the target vertex
-     * @return {@code true} if the edge-weighted digraph has a path from source vertex to v,
-     * {@code false} otherwise
+     * @return {@code true} if there is a path, {@code null} otherwise
      */
     public boolean hasPathTo(int v) {
         validateVertex(v);
-        return distTo[v] < Double.POSITIVE_INFINITY;
+        return distTo[v] > Double.NEGATIVE_INFINITY;
     }
 
     /**
-     * Returns the distance if
-     * the edge-weighted digraph has a path from vertex to {@code v}.
-     *
+     * Returns the distance from the source vertex to {@code v}.
      * @param v the target vertex
-     * @return the distance if the edge-weighted digraph has a path from vertex to v,
-     * {@code Double.POSITIVE_INFINITY} otherwise
+     * @return the distance from the source vertex to {@code v}
      */
-    public Double distTo(int v) {
+    public double distTo(int v) {
         validateVertex(v);
         return distTo[v];
     }
 
     /**
-     * Returns the edges in order of the source vertex to {@code v}
-     * if there is a path between source vertex and {@code v},
-     * {@code null} otherwise.
-     *
+     * Returns the edges on the longest path from source vertex to {@code v}.
      * @param v the target vertex
-     * @return the edges in order of the source vertex to {@code v}
-     * if there is a path between source vertex and {@code v},
-     * {@code null} otherwise
+     * @return the edges on the longest path from source vertex to {@code v}
+     * if the path exists, {@code null} otherwise
      */
     public Iterable<DirectedEdge> pathTo(int v) {
         validateVertex(v);
         Stack<DirectedEdge> path = null;
         if (hasPathTo(v)) {
-             path = new Stack<>();
-             for(DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
-                 path.push(e);
-             }
+            path = new Stack<>();
+            for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+                path.push(e);
+            }
         }
         return path;
     }
@@ -100,12 +101,12 @@ public class AcyclicSP {
         In in = new In(args[0]);
         EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
         int s = Integer.parseInt(args[1]);
-        AcyclicSP sp = new AcyclicSP(G, s);
+        AcyclicLP lp = new AcyclicLP(G, s);
 
         for(int v = 0; v < G.V(); v++) {
-            if (sp.hasPathTo(v)) {
-                StdOut.printf("%d to %d (%.2f)", s, v, sp.distTo(v));
-                for(DirectedEdge e : sp.pathTo(v)) {
+            if (lp.hasPathTo(v)) {
+                StdOut.printf("%d to %d (%.2f)", s, v, lp.distTo(v));
+                for(DirectedEdge e : lp.pathTo(v)) {
                     StdOut.print("  " + e);
                 }
                 StdOut.println();
