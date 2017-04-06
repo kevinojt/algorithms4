d16960a1b8824b92a16a2685716952d09fe0ba16
oujt <kevinojt@hotmail.com>
2017-04-06 23:07:36 +0800

    Add LSD class for sorting strings and integers with Least-Significant-Digit-first algorithm.

--- src/main/java/me/alivecode/algs4/LSD.java
+++ src/main/java/me/alivecode/algs4/LSD.java
@@ -3,13 +3,22 @@ package me.alivecode.algs4;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Created by oujt on 17-4-5.
+ * The {@code LSD} class provides static methods for
+ * sorting string array and 32-bit integer array with
+ * Least-Significant-Digit-first radix sort.
  */
 public class LSD {
     private static final int BITS_PER_BYTE = 8;
 
     private LSD() {}
 
+    /**
+     * Rearranges the string array in ascending order.
+     * All strings in the given array must have same length.
+     * @param a the array to be sorted
+     * @throws IllegalArgumentException if strings
+     * in the array has different length
+     */
     public static void sort(String[] a) {
         final int n = a.length;
         final int w = a[0].length();
@@ -23,13 +32,19 @@ public class LSD {
         sort(a, w);
     }
 
+    /**
+     * Rearranges strings in the given array in ascending order.
+     * @param a the given string array.
+     *          All strings must longer than or equal to w
+     * @param w <em>w</em>th to 0 characters to be compared
+     */
     public static void sort(String[] a, int w) {
         final int n = a.length;
         final int R = 256; // for extended ascii
 
         for(int i = 0; i < n; i++) {
             if (a[i].length() < w) {
-                throw new IllegalArgumentException("Length of strings in a[] must longer than equal to " + w);
+                throw new IllegalArgumentException("Length of strings in a[] must longer than or equal to " + w);
             }
         }
 
@@ -56,6 +71,11 @@ public class LSD {
         }
     }
 
+    /**
+     * Rearranges the array of 32-bit integer in ascending order.
+     * This is about 2x-3x faster than Arrays.sort()
+     * @param a the array to be sorted
+     */
     public static void sort(int[] a) {
         final int BITS = 32;
         final int w = BITS / BITS_PER_BYTE;
@@ -76,6 +96,8 @@ public class LSD {
                 count[r+1] += count[r];
             }
 
+            // 0x8000-0xFFFF is less than 0x0000-0x7FFF in
+            // the left most byte of signed integers
             if (d == w - 1) {
                 int shift1 = count[R] - count[R/2];
                 int shift2 = count[R/2];
@@ -101,6 +123,7 @@ public class LSD {
         }
     }
 
+    // unit test code
     public static void main(String[] args) {
 
         StdOut.println("Sorting integer-------------------------------------");
