9a5cdbd0c8752ceddf7e98531d28ddee52607872
kevinojt <kevinojt@hotmail.com>
2017-07-24 20:04:56 +0800

    I can't remember what have I done after so long :(

---
+++ src/main/java/me/alivecode/algs4/BruteForce.java
@@ -0,0 +1,7 @@
+package me.alivecode.algs4;
+
+/**
+ * Created by oujt on 14/04/2017.
+ */
+public class BruteForce {
+}
---
+++ src/main/java/me/alivecode/algs4/KMP.java
@@ -0,0 +1,7 @@
+package me.alivecode.algs4;
+
+/**
+ * Created by oujt on 15/04/2017.
+ */
+public class KMP {
+}
